f0417cd0e6419ddcde1176ba28ba85802abc8061
Mish <mishuct26@gmail.com>
2019-09-29 04:41:05 +0200

    Creates placeholder input search area

---
+++ src/components/InputSearchArea/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class InputSearchArea extends React.Component {
+    render() {
+        return (
+            <div>
+                Search Input Area
+            </div>
+        )
+    }
+
+}
+
+export default InputSearchArea;
