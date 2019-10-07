5c09b426e10fbc836aac966b88f3e2939b732464
Mish <mishuct26@gmail.com>
2019-10-07 07:40:28 +0200

    Additional property values to input

--- src/components/InputSearchArea/index.js
+++ src/components/InputSearchArea/index.js
@@ -9,10 +9,16 @@ class InputSearchArea extends React.Component {
         height: '30px',
         borderRadius: 8
     };
-
+ 
         return (
             <div>
-                <input type='text' placeholder='Enter url and Press Enter' style={inputStyling}/>
+                <input 
+                 type='text' 
+                 placeholder='Enter url and Press Enter' 
+                 style={inputStyling}
+                 className="show-value"
+                 name="website"
+                 />
             </div>
         )
     }
