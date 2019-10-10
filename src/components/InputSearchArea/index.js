92bd7992c63c121a79419e46a5dcafab6cf26c4e
Mish S <mishuct26@gmail.com>
2019-10-10 21:38:32 +0200

    Sets initial state for valid and renames value url state

--- src/components/InputSearchArea/index.js
+++ src/components/InputSearchArea/index.js
@@ -1,33 +1,44 @@
 import React from 'react'
-import $ from 'jquery'
+
 
 class InputSearchArea extends React.Component {
     constructor(props) {
         super(props);
-            // create a ref to store the textInput DOM element
-            this.textInput = null;
-            this.inputRef = props;
-        
-            this.setTextInputRef = element => {
-                this.textInput = element;
-              };
-          
+        this.state = {
+            url_value: '',
+            valid: true,
+        }
+
+            this.handleChange = this.handleChange.bind(this);
+            //   this.handleSubmit = this.handleSubmit.bind(this); 
+            // this.handleEmailChange = this.handleEmailChange.bind(this)
       }
 
-      focusTextInput = () => {
+       
+        handleChange = (event) => {
+      
         // Focus the text input using the raw DOM API
-        if (this.textInput){ this.textInput.focus();
-                // let keycode = '',
-                // if (keycode === '13') {
-             var name=$("input[name=website]").val();
-             window.open(name);
+      
+        const emailValid = this.validateEmail(url_value) 
+  
+
+        if (event.keyCode && event.keyCode === 13) {
+            if (this.state.valid) {
+              
+              window.open(`http://${this.state.value}`);
+        
+
             }
+            
+        }
+
+        
       };
     
-      componentDidMount() {
-        // autofocus the input on mount
-        this.focusTextInput();
-      }
+      componentDidMount(){
+        // document.addEventListener("keydown", this.handleEnter, false);
+        document.addEventListener("keydown", this.handleChange, false);
+    }
 
     render() {
       const inputStyling = {
@@ -35,34 +46,38 @@ class InputSearchArea extends React.Component {
         height: '30px',
         borderRadius: 8,
         marginRight: '2px'
-    };
+      };
 
-      const btnStyling = {
-        minWidth: '12vw',
-        height: '35px',
-        borderRadius: 5
-    };
+    
+    var fieldContainerClass = 'field-container'
+    const { valid } = this.state
+    
+    if (!valid) {
+      fieldContainerClass += ' error'
+    }
+
+  //   if (event.keyCode && event.keyCode === 13) {
+  //     window.open(`http://${this.state.value}`)
+  // }
 
         return (
             <div>
      {/* tell React that we want to associate the <input> ref
      with the `textInput` that we created in the constructor */}
-                <input 
-                    type='url' 
-                    placeholder='Enter url and Press Enter' 
-                    ref={this.setTextInputRef}
-                    style={inputStyling}
-                    className="show-value"
-                    name="website"
-                    required pattern="https?://.*"
-                 />
-                 <input
-                    type="button"
-                    value="Click for Enter"
-                    onClick={this.focusTextInput}
-                    style={btnStyling}
-                />
-            </div>
+
+
+<div className={fieldContainerClass}>
+        <input type='url'
+       
+          ref={this.setTextInputRef}
+          style={inputStyling}
+          onChange={this.handleChange}
+          required pattern="https?://.*"
+
+           />
+ 
+      </div>
+ </div>
         );
     }
 }
