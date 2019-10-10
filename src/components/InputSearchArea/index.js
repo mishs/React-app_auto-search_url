2d37c7bc5014bea604689a6abb4260f1cee2d871
Mish S <mishuct26@gmail.com>
2019-10-10 21:40:56 +0200

    Creates url validation function, and sets container for error as variable in handleChange function

--- src/components/InputSearchArea/index.js
+++ src/components/InputSearchArea/index.js
@@ -14,11 +14,22 @@ class InputSearchArea extends React.Component {
             // this.handleEmailChange = this.handleEmailChange.bind(this)
       }
 
-       
+    validateEmail (url_value) {
+      const regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+        if (regexp.test(url_value))
+        {
+          return this.setState({valid: true});
+        }
+        else
+        {
+          return this.setState({valid: false});
+        }
+    }
+            
         handleChange = (event) => {
-      
+          var fieldContainerClass = 'field-container'
         // Focus the text input using the raw DOM API
-      
+        const url_value = event.target.value
         const emailValid = this.validateEmail(url_value) 
   
 
@@ -26,12 +37,15 @@ class InputSearchArea extends React.Component {
             if (this.state.valid) {
               
               window.open(`http://${this.state.value}`);
-        
+              return fieldContainerClass
 
             }
             
         }
-
+        this.setState(
+          {url_value: event.target.value,
+            valid: emailValid
+          });
         
       };
     
